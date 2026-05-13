b5f4737e436072c78fd17c8cb22ca5d839a2c5d3
Bunnyfight722 <3011886148@qq.com>
2026-05-13 12:38:36 +0800

    Add files via upload

---
+++ leetcode_practice/45两数相加.js
@@ -0,0 +1,27 @@
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val);
+  this.next = (next === undefined ? null : next);
+}
+
+function addTwoNumbers(l1, l2) {
+  const dummy = new ListNode(0);
+  let current = dummy;
+  let carry = 0; // 进位
+
+  while (l1 !== null || l2 !== null || carry > 0) {
+    const val1 = l1 ? l1.val : 0;
+    const val2 = l2 ? l2.val : 0;
+
+    const sum = val1 + val2 + carry;
+    carry = Math.floor(sum / 10); // 计算新进位
+    const newVal = sum % 10; // 当前节点值
+
+    current.next = new ListNode(newVal);
+    current = current.next;
+
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+
+  return dummy.next;
+}
